8789163bbf4658e0fbd74d6b513c0e8d0c2512cb
thai_nguyen <nguyenhoathai.nht@gmail.com>
2026-02-24 20:26:47 -0800

    Added another test case to cover all path from CFD, added comments

--- ScholarshipEligibility.java
+++ ScholarshipEligibility.java
@@ -45,22 +45,53 @@ public class ScholarshipEligibility {
     }
 
     public static void main(String[] args) {
-        // Test the function
+        /**
+         * Test Case 1: "Dean consideration"
+         * Age (22) check FALSE
+         * Residency not met: ALL FALSE
+         * Income (3000 < 5000): True
+         * Return: Defer to Dean
+         */
         String test_result_consideration = 
             evaluateEligibility(22, false, 
                 false, false, false, 3000);
-
+        
+        /**
+         * Test Case 2: Age Fail
+         * Age (25) check False
+         * The other fields are randomly input
+         * Return: Not eligible
+         */
         String test_result_false = 
             evaluateEligibility(25, true, 
                 true, true, false, 6000);
 
+        /**
+         * Test casse 3: Residency met
+         * Age (19) check True
+         * Residency met: more than one condition for residency is true
+         * Income is not evaluated
+         * Return: Eligible
+         */
         String test_result_true = 
             evaluateEligibility(19, true, 
-                true, false, false, 2500);
+                true, true, false, 2500);
+
+        /**
+         * Test Case 4: Not Eligible
+         * Age (20) check True
+         * Residency not met: all conditions are False
+         * Income (6000 > 5000): False
+         * Return Not Eligible
+         */
+        String test_result_false2 = ScholarshipEligibility.evaluateEligibility(20, false, 
+                false, false, false, 6000
+);
 
         // Print the result
         System.out.println(test_result_consideration);
         System.out.println(test_result_false);
         System.out.println(test_result_true);
+        System.out.println(test_result_false2);
     }
 }
