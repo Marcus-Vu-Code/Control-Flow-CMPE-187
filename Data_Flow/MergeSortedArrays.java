ef3c5c7d45c1a70262980d46f733357fd89f6a23
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-14 14:21:37 -0700

    added more test case and adjust return type for printArray()

--- Data_Flow/MergeSortedArrays.java
+++ Data_Flow/MergeSortedArrays.java
@@ -23,7 +23,6 @@ public class MergeSortedArrays {
             }
             k++;
         }
-        
         // Move any remaning of A and B into S
         while (i < n) {
             C[k] = A[i];
@@ -35,14 +34,12 @@ public class MergeSortedArrays {
             j++;
             k++; 
         }
-        
         // Return sorted array S
         return C;
     }
-
     //Helper to covnert array to String and print it
-    public static void printArrayString(int[] arrayC) {
-        System.out.println(Arrays.toString(arrayC));
+    public static String printArrayString(int[] arrayC) {
+        return Arrays.toString(arrayC);
     }
 
     public static void main(String[] args) {
@@ -54,14 +51,41 @@ public class MergeSortedArrays {
         int[] C = merge(A, B);
         
         // Print result
-        System.out.println("Input Arrays:");
-        System.out.print("A = ");
-        printArrayString(A);
-        System.out.print("B = ");
-        printArrayString(B);
+        System.out.printf("A = %s\nB = %s\nC = %s\n\n", 
+            printArrayString(A), printArrayString(B), printArrayString(C));
 
-        System.out.println("Output Array:");
-        System.out.print("C = ");
-        printArrayString(C);   
+        // MORE TEST CASES
+        int[] A2 = {};
+        int[] B2 = {};
+        int[] C2 = merge(A2, B2);
+        System.out.printf("A2 = %s\nB2 = %s\nC2 = %s\n\n", 
+            printArrayString(A2), printArrayString(B2), printArrayString(C2));
+
+        int[] A3 = {};
+        int[] B3 = {1, 3, 5};
+        int[] C3 = merge(A3, B3);
+        System.out.printf("A3 = %s\nB3 = %s\nC3 = %s\n\n", 
+            printArrayString(A3), printArrayString(B3), printArrayString(C3));
+
+        int[] A4 = {2,4,6};
+        int[] B4 = {};
+        int[] C4 = merge(A4, B4);
+        System.out.printf("A4 = %s\nB4 = %s\nC4 = %s\n\n", 
+            printArrayString(A4), printArrayString(B4), printArrayString(C4));
+
+        int[] A5 = {1,3,5};
+        int[] B5 = {2,4,6};
+        int[] C5 = merge(A5, B5);
+        System.out.printf("A5 = %s\nB5 = %s\nC5 = %s\n\n", 
+            printArrayString(A5), printArrayString(B5), printArrayString(C5));
+        
+        int[] A6 = {-5, 0, 2};
+        int[] B6 = {-3,1,4};
+        int[] C6 = merge(A6, B6);
+        System.out.printf("A6 = %s\nB6 = %s\nC6 = %s\n\n", 
+            printArrayString(A6), printArrayString(B6), printArrayString(C6));
+        
+        
+
     }
 }
